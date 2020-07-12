d81a84d3f6f4cdfe5277f85534d94bd3164d5f7d
Matt <matthewjamesrea@gmail.com>
2020-07-11 19:48:46 -0500

    added some basic data validation and a few comments

--- app/src/main/java/com/mrea/weatherapp/data/AuthInterceptor.kt
+++ app/src/main/java/com/mrea/weatherapp/data/AuthInterceptor.kt
@@ -5,6 +5,12 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 object AuthInterceptor : Interceptor {
+
+    // Using API key-based authentication to save time.
+    // A more secure approach would be OAuth2, which the Here API does support
+
+    // Additionally, some form of protection against man-in-the-middle attacks should be used,
+    // such as SSL Pinning or Certificate Transparency
     private const val API_KEY_NAME: String = "apiKey"
     private const val API_KEY: String = "QK_yM83wVXTpObnyknsVr1Uq7brPnNOYSjFM3-WrRSQ"
 
--- app/src/main/java/com/mrea/weatherapp/domain/GetCurrentWeather.kt
+++ app/src/main/java/com/mrea/weatherapp/domain/GetCurrentWeather.kt
@@ -5,6 +5,15 @@ import com.mrea.weatherapp.data.WeatherRepository
 class GetCurrentWeather(private val repository: WeatherRepository) {
 
     suspend fun run(zipCode: String, refresh: Boolean = false): Weather {
-        return repository.getWeather(zipCode, refresh)
+        val weather = repository.getWeather(zipCode, refresh)
+
+        // todo: mrea 7/11/20 - review business requirements for error handling with Product Owner
+        if (weather.city.isEmpty() || weather.state.isEmpty() || weather.description.isEmpty()) {
+            throw InvalidNetworkResponse()
+        }
+
+        return weather
     }
+
+    class InvalidNetworkResponse: Exception("The response from the API is missing required data")
 }
